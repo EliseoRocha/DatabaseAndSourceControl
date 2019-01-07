dab02080ed91db02b0a7d9761a5e6a0f82602309
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-01-07 14:43:59 -0800

    Added connection to server.

---
+++ DatabaseAndSourceControl/CustomerDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAndSourceControl
+{
+    static class CustomerDB
+    {
+        public static List<string> GetAllNames()
+        {
+            //
+            string connection = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=CustomerDB;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+
+                //TODO; finish database code...
+            }
+
+            //Same as above, but above doesn't do exception handling
+            //SqlConnection con = new SqlConnection(connection);
+
+            //try
+            //{
+            //    con.Open();
+            //}
+            //finally 
+            //{
+
+            //    con.Dispose();
+            //}
+        }
+    }
+}
