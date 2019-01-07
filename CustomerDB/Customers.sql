8d62adc0a3fa28b316812a215ca0c0b2d508b124
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-01-07 14:19:50 -0800

    Created the Customer table and added some test data.

--- CustomerDB/Customers.sql
+++ CustomerDB/Customers.sql
@@ -1,1 +1,13 @@
 ﻿
+CREATE TABLE Customer
+(
+	--IDENTITY key starts at 1 and incements
+	ID		int			Primary Key	IDENTITY
+	,Name	varchar(30)	NOT NULL
+)
+
+INSERT INTO Customer 
+VALUES ('Joe')
+	, ('Ken')
+	, ('Jim')
+	, ('Pam')
